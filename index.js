a4e1c4c9340c3cb45bfbc737b3e8cdbc9b2ce809
Zainab Azeem <zazeem159@gmail.com>
2026-01-28 10:56:58 +0500

    update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 5000;
 //   console.log(`✅ Server running on http://localhost:${PORT}`);
 // });
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log('Server running');
 });
