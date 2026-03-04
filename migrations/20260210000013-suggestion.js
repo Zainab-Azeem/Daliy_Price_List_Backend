dfa3faa43c137875fd27b58b450240bc2f84d082
Zainab Azeem <zazeem159@gmail.com>
2026-03-04 13:08:20 +0500

    add new table suggestion

---
+++ migrations/20260210000013-suggestion.js
@@ -0,0 +1,92 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+   async up (queryInterface, Sequelize) {
+     await queryInterface.createTable("suggestion", {
+      product_id: {
+        type: Sequelize.CHAR(36),
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.literal("uuid()"),
+      },
+
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        defaultValue: null,
+      },
+
+      price: {
+        type: Sequelize.DECIMAL(10,2),
+        allowNull: false,
+      },
+
+      discount_percent: {
+        type: Sequelize.DECIMAL(5,2),
+        allowNull: true,
+        defaultValue: 0.00,
+      },
+
+      stock_qty: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+
+      image_url: {
+        type: Sequelize.STRING(500),
+        allowNull: true,
+        defaultValue: null,
+      },
+
+      category: {
+        type: Sequelize.STRING(100),
+        allowNull: true,
+        defaultValue: null,
+      },
+
+      is_active: {
+        type: Sequelize.BOOLEAN, 
+        allowNull: true,
+        defaultValue: 1,
+      },
+
+      approval_status: {
+          type: Sequelize.ENUM("pending", "approved", "rejected"),
+          allowNull: false,
+          defaultValue: "pending",
+        },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("current_timestamp()"),
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("current_timestamp()"),
+      },
+
+
+     },
+     {
+  charset: "utf8mb4",
+  collate: "utf8mb4_unicode_ci",
+});
+
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('suggestion');
+     
+  }
+};
